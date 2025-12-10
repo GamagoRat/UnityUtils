0e54d27847b5c56e2934f962123b8f579f457ec4
Lucas SPITZ DREVET <48288304+Yodwaae@users.noreply.github.com>
2025-12-10 15:08:37 +0100

    Minimal implementation

--- Assets/Utils/AnimSystem/Scripts/CustomAnimator.cs
+++ Assets/Utils/AnimSystem/Scripts/CustomAnimator.cs
@@ -13,12 +13,26 @@ public class CustomAnimator : MonoBehaviour
 
     public void ChangeAnimationState(string newState)
     {
-
         // Early exit
         if (currentState == newState)
             return;
+
         // Play the animation
+        currentState = newState;
         animator.Play(newState);
+    }
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+            ChangeAnimationState("BreathingIdle");
+
+        if (Input.GetKeyDown(KeyCode.R))
+            ChangeAnimationState("FastRun");
+
+        if (Input.GetKeyDown(KeyCode.W))
+            ChangeAnimationState("Walking");
     }
 
 }
