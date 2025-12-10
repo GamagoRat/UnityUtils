3a2f32458ee801fe9c9a755e884f2568f3347935
Lucas SPITZ DREVET <48288304+Yodwaae@users.noreply.github.com>
2025-12-10 16:35:43 +0100

    Added attack for example of implementation with call back
    - Need to fix callback delay as old approach doesn't work anymore

--- Assets/Utils/AnimSystem/Scripts/CustomAnimator.cs
+++ Assets/Utils/AnimSystem/Scripts/CustomAnimator.cs
@@ -2,17 +2,23 @@ using UnityEngine;
 
 public class CustomAnimator : MonoBehaviour
 {
+    // NOTE : In optimals conditions the same script wouldn't have both responsabilities (polling input and animation management) but for example sake it's simpler
+    // Also the transitions between the animation pos are kinda violent because that's just random mixamo anims and not anims made to work together
 
+    // Animation
     private Animator animator;
     private string currentState;
+    private float animationDelay;
+
+    // Flags
+    private bool isAttacking;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
     }
 
-    public void ChangeAnimationState(string newState)
-    {
+    public void ChangeAnimationState(string newState) {
         // Early exit
         if (currentState == newState)
             return;
@@ -22,7 +28,7 @@ public class CustomAnimator : MonoBehaviour
         animator.Play(newState);
     }
 
-
+  
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.B))
@@ -33,6 +39,32 @@ public class CustomAnimator : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.W))
             ChangeAnimationState("Walking");
+        
+        if (!isAttacking && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+
+            isAttacking = true;
+            ChangeAnimationState("Attack");
+
+            // Set up callback
+            animationDelay = animator.GetCurrentAnimatorStateInfo(0).length; // FIXME Doesn't work anymore as the animation change in the next frame
+            Invoke("AttackComplete", animationDelay);
+        }
+    }
+
+    // TODO NEED To change that to get the anim lenght instead of just debug
+    private void GetClipLength()
+    {
+        foreach (var clip in animator.runtimeAnimatorController.animationClips)
+        {
+            if (clip.name == "Attack")
+                Debug.Log(clip.length);
+        }
+    }
+
+    private void AttackComplete() {
+        isAttacking = false;
+        ChangeAnimationState("BreathingIdle");
     }
 
 }
